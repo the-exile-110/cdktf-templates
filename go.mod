56770483fe5162c5a7a5d58f5be0ac862565cf1c
RUI <a6504130@gmail.com>
2023-07-09 21:31:22 +0900

    setup

--- go.mod
+++ go.mod
@@ -4,8 +4,19 @@ go 1.18
 
 require github.com/aws/constructs-go/constructs/v10 v10.2.65
 
-require github.com/hashicorp/terraform-cdk-go/cdktf v0.17.1
+require (
+	github.com/cdktf/cdktf-provider-aws-go/aws/v10 v10.0.12
+	github.com/hashicorp/terraform-cdk-go/cdktf v0.17.1
+)
 
-require github.com/aws/jsii-runtime-go v1.84.0 // indirect
+require github.com/aws/jsii-runtime-go v1.84.0
 
-require github.com/Masterminds/semver/v3 v3.2.1 // indirect
+require (
+	github.com/Masterminds/semver/v3 v3.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/yuin/goldmark v1.4.13 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/tools v0.9.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
+
+	awsProvider "github.com/cdktf/cdktf-provider-aws-go/aws/v10/provider"
 )
 
 func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 	stack := cdktf.NewTerraformStack(scope, &id)
 
-	// The code that defines your stack goes here
+	awsProvider.NewAwsProvider(stack, jsii.String("AWS"), &awsProvider.AwsProviderConfig{
+		Region: jsii.String("us-west-1"),
+	})
 
 	return stack
 }
 
 func main() {
 	app := cdktf.NewApp(nil)
+	stack := NewMyStack(app, "aws_instance")
 
-	NewMyStack(app, "cdktf-templates")
+	cdktf.NewS3Backend(stack, &cdktf.S3BackendConfig{
+		Bucket: jsii.String("cdktf-go"),
+		Key:    jsii.String("terraform.tfstate"),
+		Region: jsii.String("ap-northeast-1"),
+	})
+	fmt.Println(stack)
 
 	app.Synth()
 }
